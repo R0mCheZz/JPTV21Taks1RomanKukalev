baa712a0293e3188602012e559fc31ee8f3fdc32
pupil <pupil@ivkhk.ee>
2022-09-26 12:41:26 +0300

    JPTV21Task1RomanKukalev

--- src/jptv21task1romankukalev/JPTV21Task1RomanKukalev.java
+++ src/jptv21task1romankukalev/JPTV21Task1RomanKukalev.java
@@ -15,6 +15,7 @@ public class JPTV21Task1RomanKukalev {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         // TODO code application logic here
     }
     
